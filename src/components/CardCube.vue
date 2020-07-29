c1222a510bcc099cabc449ac504cd41bc1230e64
Vlad Tarasenko <vladsmack@gmail.com>
2020-07-30 02:32:12 +0300

    Feat: CardCube.vue (picture/painting) component

---
+++ src/components/CardCube.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="card-cube rounded" :style="`background-image: url(${bg})`">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardCube',
+    props: {
+        item: {
+            type: Object,
+            default: () => {},
+        }
+    },
+    computed: {
+        bg() {
+            return require(`../assets/img/${this.item.img}`);
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+.card-cube {
+    overflow: hidden;
+    box-shadow: $card-shadow;
+    margin-bottom: 10px;
+    cursor: pointer;
+    background-position: center;
+    background-size: cover;
+
+    &::after {
+        display: block;
+        content: "";
+        padding-top: 100%;
+        width: 100%;
+        pointer-events: none;
+    }
+
+    @include up($md) {
+        margin-bottom: 20px;
+    }
+}
+</style>
